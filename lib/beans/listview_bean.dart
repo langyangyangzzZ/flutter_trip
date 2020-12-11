bb1bcc8b3ca086d4168a400fc131f86912faaa5a
86177 <562520643@qq.com>
2020-12-11 17:27:00 +0800

    Flutter ListView使用

--- lib/beans/listview_bean.dart
+++ lib/beans/listview_bean.dart
@@ -1,7 +1,8 @@
 
 class ListViewBean{
+  //是否选中
   bool ischeck = false;
+  //标题
   String title;
-
-  ListViewBean(this.ischeck, this.title);
+  ListViewBean(this.ischeck, this.title,);
 }
--- lib/tests/listview_text.dart
+++ lib/tests/listview_text.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_trip/beans/listview_bean.dart';
 import 'package:flutter_trip/util/loading_util.dart';
 import 'package:flutter_trip/util/log_util.dart';
@@ -18,55 +19,27 @@ class _ListViewTextWidgetState extends State<ListViewTextWidget> {
   void initState() {
     super.initState();
 
-    for (int i = 0; i <= 20; i++) {
+    for (int i = 0; i <= 100; i++) {
       _list.add(new ListViewBean(false, "$i"));
     }
   }
 
+  int _type = 0;
+
   List<Widget> initData() {
     List<Widget> mlist = [];
+
+    mlist.add(initButton("单选", 1));
+    mlist.add(initButton("多选", 2));
+    mlist.add(initButton("全选", 3));
+
     _list.forEach((e) => {
           mlist.add(
             isLoadingImage
-                ? Container(
-                    height: 100,
-                    alignment: Alignment.center,
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: Text("第${e.title}个元素"),
-                        ),
-                        RaisedButton(
-                          color: e.ischeck ? Colors.black : Colors.red,
-                          onPressed: () {
-                            setState(() {
-
-                              _list.forEach((element) {
-                                e.ischeck  =   element.ischeck =  !element.ischeck;
-                              });
-
-                              // //多选
-                              // if (!e.ischeck) {
-                              //   _lsitcheck.add("${e.title}");
-                              // } else {
-                              //   _lsitcheck.remove("${e.title}");
-                              // }
-                              // Toast.toast(context,
-                              //     msg: "${_lsitcheck.toString()}");
-                              //
-                              // e.ischeck = !e.ischeck;
-                            });
-                          },
-                          child: Text(
-                            e.ischeck ? "已收藏" :"未收藏",
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        )
-                      ],
-                    ),
-                  )
-                : Container(
-                    height: 100,
+                ? buildItem(e)
+                :
+                //滑动时显示的LoadingUtil
+                Container(
                     alignment: Alignment.center,
                     child: LoadingUtil.loading(context),
                   ),
@@ -75,14 +48,16 @@ class _ListViewTextWidgetState extends State<ListViewTextWidget> {
     return mlist;
   }
 
-  bool isLoadingImage = true;
+  bool isLoadingImage = true; //是否滑动
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         child: NotificationListener(
-          child: _buildListView(),
+          child: Container(
+            child: _buildListView(),
+          ),
           onNotification: (notification) {
             ///通知类型
             switch (notification.runtimeType) {
@@ -114,9 +89,147 @@ class _ListViewTextWidgetState extends State<ListViewTextWidget> {
     );
   }
 
+  ScrollController scrollController = ScrollController();
+
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
   Widget _buildListView() {
+      scrollController.addListener(() {
+      ///scrollController.position.maxScrollExtent  ListView滑动的最大距离
+      ///scrollController.position.pixels  现在滑动的距离
+      if (scrollController.position.maxScrollExtent ==
+          scrollController.position.pixels) {
+        LogUtil.Log(
+            tagging: "scrollController:",
+            title:
+                "pixels:${scrollController.position.pixels}maxScrollExtent:${scrollController.position.maxScrollExtent}");
+      }
+    });
     return ListView(
+      //滑动方向
+      scrollDirection: Axis.vertical,
+
+      //当scrollDirection = Axis.vertical  itemExtent代表的是高度
+      //当scrollDirection = Axis.horizontal  itemExtent代表的是宽度
+      itemExtent: 50,
+
+      //内边距
+      padding: EdgeInsets.only(left: 10, right: 10, top: 30),
+
+      //当条目不足时 true可以尝试滚动  false不可以滚动
+      primary: false,
+
+      physics: ClampingScrollPhysics(),
+      ///BouncingScrollPhysics 拉到ListView最底部有回弹效果
+      ///NeverScrollableScrollPhysics 滑动禁止
+      ///ClampingScrollPhysics  包裹内容不会回弹
+
+      // 预加载区域
+      cacheExtent: 0.0,
+
+      //是否倒序显示 默认正序 false  倒序true
+      reverse: false,
+      ///使用ScrollController scrollController = ScrollController();
+      ///对ListView监听,多用于监听是否滚动到底部
+      controller: scrollController,
+
+      //子Widget
       children: initData(),
     );
+  }
+
+  initButton(String title, int type) {
+    return ElevatedButton(
+      onPressed: () {
+        setState(() {
+          //将所有的默认为为收藏
+          _list.forEach((element) {
+            element.ischeck = false;
+          });
+
+          _type = type;
+        });
+      },
+      child: Text(title),
+    );
+  }
+
+  ///[e] 当前的Widget
+  Widget buildItem(ListViewBean e) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(color: Colors.lightBlueAccent,width: 0.5)
+        )
+      ),
+      alignment: Alignment.center,
+      child: Row(
+        children: [
+          Expanded(
+            child: Text("第${e.title}个元素"),
+          ),
+          RaisedButton(
+            color: e.ischeck ? Colors.black : Colors.red,
+            onPressed: () {
+              setState(() {
+                if (_type == 1) {
+                  //单选
+                  initSingleChoice(e);
+
+                } else if (_type == 2) {
+                  //多选
+                  initMultipleChoice(e);
+                } else {
+                  //全选
+                  _list.forEach((element) {
+                    e.ischeck = element.ischeck = !element.ischeck;
+                  });
+                }
+              });
+            },
+            child: Text(
+              e.ischeck ? "已收藏" : "未收藏",
+              style: TextStyle(color: Colors.white),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  //单选
+  void initSingleChoice(ListViewBean e) {
+    //单选
+    e.ischeck = !e.ischeck;
+    _list.forEach((element) {
+      //若当前有选中的按钮
+      if (element.ischeck == e.ischeck && e.ischeck == true) {
+        print("SZJischeck${element.ischeck }title:${element.title}");
+        //无论有没有选中 都将改成未选中
+        element.ischeck = false;
+        //选中
+        e.ischeck = true;
+
+        Toast.toast(context, msg: e.title);
+      }
+    });
+  }
+
+  //多选
+  void initMultipleChoice(ListViewBean e) {
+    //多选
+    if (!e.ischeck) {
+      _lsitcheck.add("${e.title}");
+    } else {
+      _lsitcheck.remove("${e.title}");
+    }
+    Toast.toast(context, msg: "${_lsitcheck.toString()}");
+
+    e.ischeck = !e.ischeck;
   }
 }
